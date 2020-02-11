36e0f270c8a2522ad73a768ad6fc909165d34a45
Brandon-Amezcua <brandonamezcua@csu.fullerton.edu>
2020-02-10 17:47:14 -0800

    started the convert function

--- AsciiConverter.c
+++ AsciiConverter.c
@@ -1,10 +1,53 @@
 #include <stdio.h>
 #include <math.h>
+#include <bool.h>
+
+#include "fileread.h"
+
+int convert(char* string[], int size);
 
 int main(int argc, const char* argv[]) {
   const char* names[] = {"NUL", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL", "BS",
   "TAB", "LF", "VT", "FF", "CR", "SO", "SI","DLE", "DC1", "DC2", "DC3", "DC4",
   "NAK", "SYN", "ETB", "CAN", "EM", "SUB", "ESC", "FS", "GS", "RS", "US"};
 
-  
+  const char* parity[] = {"Even", "Odd"};
+
+  FILE* fin;
+  FILE* fout;
+  int n;
+  int code;
+
+  char* asc = "01000001";
+  int deci = convert(asc);
+
+  /*
+  bool ok = openfile(argc, argv, &fin, &fout, n);
+  if (!ok) {exit(1);}
+
+  char* s[n];
+
+  while (fgets(s[], n, fin) != EOF) {
+    code = convert(s);
+  }
+  */
+
+
+  printf("Original\tASCII\tDcimal\t");
+  printf("%s\t%c\t%\t%d\t%s",);
+
+
+}
+
+
+int convert(char* string[]) {
+  int j = 0;
+  int decimal = 0;
+  for (int i = strlen(s); i <= 0; i--) {
+    if (s[i] == '1'){
+      decimal += pow(2,j);
+    }
+    j++;
+  }
+  return decimal;
 }
