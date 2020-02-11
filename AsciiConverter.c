26462c753db49d1564d7acdbbbf0d138e0728c73
Brandon-Amezcua <42852698+Brandon-Amezcua@users.noreply.github.com>
2020-02-10 16:57:23 -0800

    Add files via upload

---
+++ AsciiConverter.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char* argv[]) {
+  const char* names[] = {"NUL", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL", "BS",
+  "TAB", "LF", "VT", "FF", "CR", "SO", "SI","DLE", "DC1", "DC2", "DC3", "DC4",
+  "NAK", "SYN", "ETB", "CAN", "EM", "SUB", "ESC", "FS", "GS", "RS", "US"};
+
+  
+}
